4562d14a9e8fde19ade85ce4396285a6e24bcd32
chris <chris@DESKTOP-AMOTF7V>
2023-10-31 22:35:38 -0400

    Navbar design

---
+++ Client/src/Components/Header/Nav.jsx
@@ -0,0 +1,12 @@
+const Nav = () => {
+    return(
+        <nav className="header-nav__content">
+            <p className="header-nav__content__item">Home</p>
+            <p className="header-nav__content__item">Catalog</p>
+            <p className="header-nav__content__item">Contacts</p>
+            <p className="header-nav__content__item">About Us</p>
+        </nav>
+    )
+};
+
+export default Nav;
--- Client/src/Components/Header/index.jsx
+++ Client/src/Components/Header/index.jsx
@@ -1,23 +1,28 @@
 import "./Header.css"
+import {PiShoppingCartSimpleBold} from 'react-icons/pi'
+import { LuSearch } from "react-icons/lu"
+import {VscAccount} from 'react-icons/vsc'
+import Nav from "./Nav";
 
 const Header = () => {
-    return(
-        <header className="header">
-            <img className="header__logo" src="img/GardenBloom-logo.png" alt="" />
 
-            <form action="" className="header-form__content">
-                <input type="search" className="header-form__content__input" />
-                <button type="submi" className="header-form__content__btn">Buscar
-                </button>
-            </form>
-            <nav className="header-nav__content">
-                <p>Home</p>
-                <p>Catalog</p>
-                <p>Contacts</p>
-                <p>About Us</p>
-                <p>Car</p>
-                <p>My Account</p>
-            </nav>
+    return(
+        <header>
+           <div className="header">
+                <img className="header__logo" src="img/GardenBloom-logo.png" alt="" />
+
+                <form action="" className="header-form__content">
+                    <input type="search" className="header-form__content__input" />
+                    <button type="submi" className="header-form__content__btn"><LuSearch />
+                    </button>
+                </form>
+                <PiShoppingCartSimpleBold className="header__icon"
+                    size="1.5rem"
+                    color="pink" />
+                <VscAccount className="header__icon" color="pink" 
+                size="1.5rem"/>
+           </div>
+
         </header>
     );
 };
